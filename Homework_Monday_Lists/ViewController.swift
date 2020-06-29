04286fd1e0d857e2ba48810e57e2d49694785058
Neil <neil@powertendevelopment.com>
2020-06-29 15:47:55 -0500

    Initial Commit

---
+++ Homework_Monday_Lists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework_Monday_Lists
+//
+//  Created by Neil Bergenroth on 6/29/20.
+//  Copyright © 2020 Neil Bergenroth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
